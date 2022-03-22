77c531426d3e8dd12faa50008e6c7528d5204886
EricWehrly <eric@disruptiontheory.com>
2022-03-21 21:08:34 -0400

    wander around rather than jitter

--- engine/js/ai/basic.mjs
+++ engine/js/ai/basic.mjs
@@ -1,8 +1,13 @@
 // Most basic / default AI
 // TODO: Predator and prey supertypes above this
+
+const MS_BETWEEN_WANDER_DESTINATIONS = 30000;   // 30 seconds
+
 export default class AI {
 
     #character = null;
+
+    #lastDestinationPickedTime = performance.now();
 
     constructor(character) {
         this.#character = character;
@@ -21,19 +26,43 @@ export default class AI {
         // if I have a target
         // move to it
 
+        // if predator, run towards other creatures
+        // if prey, run away from predators (including players)
+
         // if i don't
         this.wander();
+
+        this.#character.moveToTarget();
     }
 
     wander() {
-        var randX = Math.random();
+        // instead of doing this, occasionally pick a new nearby target to walk to
+        if(performance.now() - this.#lastDestinationPickedTime > MS_BETWEEN_WANDER_DESTINATIONS) {
+            console.log(`Old target: ${this.#character.target.position.x}, ${this.#character.target.position.y}`);
+            this.#lastDestinationPickedTime = performance.now();
+            this.#character.target = {
+                position: {}
+            }
+
+            var randX = Math.random();
+            if(randX > 0.5) this.#character.target.position.x = (10 * randX) - 5;
+            else this.#character.target.position.x = (-10 * randX) + 5;
+            
+            var randY = Math.random();
+            if(randY > 0.5) this.#character.target.position.y = (10 * randY) - 5;
+            else this.#character.target.position.y = (-10 * randY) + 5;
+
+            console.log(`New target: ${this.#character.target.position.x}, ${this.#character.target.position.y}`);
+        }
+
+        /*
         if(randX < 0.3) this.#character.velocity.x = -1;
         else if(randX > 0.6) this.#character.velocity.x = 1;
         else this.#character.velocity.x = 0;
         
-        var randY = Math.random();
         if(randY < 0.3) this.#character.velocity.y = -1;
         else if(randY > 0.6) this.#character.velocity.y = 1;
         else this.#character.velocity.y = 0;
+        */
     }
 }
