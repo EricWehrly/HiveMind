7c1eec6830cec4f471ce988d9e1bcae3eaeeff64
EricWehrly <eric@disruptiontheory.com>
2022-03-27 13:32:30 -0400

    initial implementation of reusable character types

---
+++ js/entities/characterType.mjs
@@ -0,0 +1,5 @@
+const CharacterType = {
+
+}
+
+export default CharacterType;
--- js/entities/food.mjs
+++ js/entities/food.mjs
@@ -1,16 +1,24 @@
 import Character from './character-extensions.mjs';
+import CharacterType from './characterType.mjs';
+
+// need a definition for character types
+// so the player can study the type and learn how to gain health from it
+
+CharacterType.Apple = {
+    color: 'green',
+    health: 5,
+    ai: null
+}
 
 for (var i = 0; i < 3; i++) {
     // random x and y within some range, on positive axis
-    let x = Math.random() * 10;
-    let y = Math.random() * 10;
-    // spawn food
-    new Character({
-        color: 'green',
+    let opts = {
         position: {
-            x: x,
-            y: y
-        },
-        ai: null
-    });
+        x: Math.random() * 10,
+        y: Math.random() * 10
+        }
+    };
+    
+    Object.assign(opts, CharacterType.Apple);
+    new Character(opts);
 }
