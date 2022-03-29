c7a36e95920a2309d819759cf4b221b28436fc81
EricWehrly <eric@disruptiontheory.com>
2022-03-28 21:38:34 -0400

    fix an issue with claws definition

--- js/technology.mjs
+++ js/technology.mjs
@@ -9,6 +9,8 @@ export default class Technology extends Listed {
     constructor(options = {}) {
 
         // TODO: reject if missing required options
+        if(!options.name) throw `Technology needs name!`;
+        if(!options.type) throw `Technology ${options.name} needs type!`;
         super(options);
     }
 }
