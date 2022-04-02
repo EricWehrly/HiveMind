cea8b780e6fbc600b79486e4d3b6913a15366fdd
EricWehrly <eric@disruptiontheory.com>
2022-04-02 19:33:05 -0400

    fill in missing method

--- engine/js/util/javascript-extensions.js
+++ engine/js/util/javascript-extensions.js
@@ -67,3 +67,7 @@ export function generateId(len) {
   window.crypto.getRandomValues(arr)
   return Array.from(arr, dec2hex).join('')
 }
+
+export function dec2hex (dec) {
+    return ('0' + dec.toString(16)).substr(-2)
+}
