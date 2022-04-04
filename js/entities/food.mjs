e3c5be4ab280ed61633ddefdc92b150bbe4178e9
EricWehrly <eric@disruptiontheory.com>
2022-04-03 23:23:08 -0400

    spawn clusters of food

--- js/entities/food.mjs
+++ js/entities/food.mjs
@@ -12,18 +12,32 @@ new CharacterType({
     ai: null
 });
 
+const number_of_food_spawn_points = 5;
+const food_per_spawn_min = 2;
+const food_per_spawn_max = 5;
+
 Events.Subscribe(Events.List.GameStart, function() {     
 
-    for (var i = 0; i < 3; i++) {
-        // random x and y within some range, on positive axis
-        let opts = {
-            position: {
-            x: Math.random() * 10,
-            y: Math.random() * 10
-            }
-        };
-        
-        Object.assign(opts, CharacterType.Apple);
-        new Character(opts);
+    for (var i = 0; i < number_of_food_spawn_points; i++) {
+
+        const spawnerPosition = {
+            x: Math.randomBetween(4, 40),
+            y: Math.randomBetween(4, 20),
+        }
+        const amountToSpawn = Math.randomBetween(food_per_spawn_min, food_per_spawn_max);
+        for(var i2 = 0; i2 < amountToSpawn; i2++) {
+            const offsetX = Math.randomBetween(-3, 3);
+            const offsetY = Math.randomBetween(-3, 3);
+
+            const opts = {
+                position: {
+                    x: spawnerPosition.x + offsetX,
+                    y: spawnerPosition.y + offsetY
+                }
+            };
+            
+            Object.assign(opts, CharacterType.Apple);
+            new Character(opts);
+        }
     }
 });
