47a1c9e817dd145dfaf523a02471d7cde1dd601a
EricWehrly <eric@disruptiontheory.com>
2022-04-11 04:45:58 -0400

    render loop methods

--- engine/js/rendering/renderer.mjs
+++ engine/js/rendering/renderer.mjs
@@ -1,6 +1,41 @@
 export default class Renderer {
 
+    static _renderMethods = [];
+    static #renderMethodPriorities = {};
+
     static Render(screenRect) {
-        console.log("am i real");
+    }
+
+    static RegisterRenderMethod(priority, method, options) {
+
+        options = Renderer.#registerRenderMethod_defaultOptions(options);
+    
+        while(priority in Renderer.#renderMethodPriorities) {
+            priority = priority + 1;
+        }
+        options.method = method;
+        Renderer.#renderMethodPriorities[priority] = options;
+    
+        Renderer.#reprioritizeRendering();
+    
+        return options.id;
+    }
+
+    static #registerRenderMethod_defaultOptions = function(options) {
+    
+        if(options == null) options = {};
+        if (!options.layer) options.layer = "main";
+        if(!options.frameLimit) options.frameLimit = -1;
+        options.id = generateId(6);
+    
+        return options;
+    }
+
+    static #reprioritizeRendering = function() {
+    
+        Renderer_renderMethods = [];
+        for(var priorityIndex in Renderer.#renderMethodPriorities) {
+            Renderer._renderMethods.push(Renderer.#renderMethodPriorities[priorityIndex]);
+        }
     }
 }
