7aa095ed347cc7d7aaf031da5e383f69ee0212d0
EricWehrly <eric@disruptiontheory.com>
2023-01-27 19:25:17 -0500

    clean up

--- js/ui/tooltip.mjs
+++ js/ui/tooltip.mjs
@@ -20,8 +20,6 @@ export default class Tooltip extends UIElement {
 
         this.message = options.message || "";
         this.visible = this.message != "";
-        
-        // Renderer.RegisterRenderMethod(10, this.redraw.bind(this));
     }
 
     get message() {
@@ -33,33 +31,7 @@ export default class Tooltip extends UIElement {
 
         this.#message = newValue;
         this.Element.innerHTML = this.message;
-        // TODO: show/hide methods?
-        if(this.#message == null || this.#message == "") this.Element.style.display = "none";
-        else {
-            // this.followEntity();
-            this.Element.style.display = "inline-block";
-        }
-    }
-
-    // TODO: No, this needs to go in the renderer
-    redraw(screenRect) {
-        if(this.message != null && this.message != ""
-            && this.entity) {
-
-            // TODO: get grid size constant
-            const gridSize = 32;
-
-            const offsetPosition = {
-                x: this.entity.position.x - screenRect.x,
-                y: this.entity.position.y - screenRect.y
-            };
-            let targetY = gridSize * offsetPosition.y;
-            if(this.entity?.graphic?.style?.height) {
-                targetY -= parseInt(this.entity.graphic.style.height);
-            }
-
-            this.Element.style.left = (gridSize * offsetPosition.x) + "px";
-            this.Element.style.top = targetY + "px";
-        }
+        
+        this.visible = this.message != "";
     }
 }
