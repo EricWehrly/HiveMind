d429b1d3ba24c83451c255476378538352e8675a
EricWehrly <eric@disruptiontheory.com>
2023-02-03 00:11:07 -0500

    avoid circular dependencies when stringifying network messages

--- js/network/network-messenger.mjs
+++ js/network/network-messenger.mjs
@@ -6,9 +6,11 @@ export default class NetworkMessenger {
     static TransmitEvent(eventName, detail) {
 
         // TODO: attach playerid metadata info to request, just in case
+
+        const newDetail =NetworkMessenger.#sanitize(detail);
         var payload = JSON.stringify({
             eventName,
-            detail
+            newDetail
         });
         // TODO: loop peerConnections instead, later, apparently
         // TODO: need to transmit a "welcome packet" of current state to any new players...
@@ -21,5 +23,26 @@ export default class NetworkMessenger {
         var parsed = JSON.parse(event);
         // if no eventName or detail, throw warning
         Events.RaiseEvent(parsed.eventName, parsed.detail);
+    }
+
+    // TODO: this is almost certainly too slow a method ...
+    static #sanitize(detail) {
+
+        // this breaks, not even really sure why
+        // const newDetail = structuredClone(detail);
+
+        const newDetail = {};
+        for(var key of Object.keys(detail)) {
+
+            if(detail[key] == null) continue;
+
+            // this is to remove circular dependencies for JSON.stringify
+            if(typeof detail[key] == 'object') {
+                if(Object.values(detail[key]).includes(detail)) continue;
+            }
+            newDetail[key] = detail[key];
+        }
+
+        return newDetail;
     }
 }
