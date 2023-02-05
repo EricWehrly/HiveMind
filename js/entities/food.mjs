a5b32e3e29737f10b1b7a6de886b9fd22f6d22eb
EricWehrly <eric@disruptiontheory.com>
2023-02-05 01:24:16 -0500

    spawn food with cunk creation!

--- js/entities/food.mjs
+++ js/entities/food.mjs
@@ -1,6 +1,7 @@
 import Character from './character-extensions.mjs';
 import CharacterType from './characterType.mjs';
 import Events from '../../engine/js/events.mjs';
+import Chunk from '../../engine/js/mapping/chunk.mjs';
 
 // need a definition for character types
 // so the player can study the type and learn how to gain health from it
@@ -12,32 +13,47 @@ new CharacterType({
     ai: null
 });
 
-const number_of_food_spawn_points = 5;
-const food_per_spawn_min = 2;
-const food_per_spawn_max = 5;
+export default class Food {
 
-Events.Subscribe(Events.List.GameStart, function() {     
+    // TODO: define this in the biome, once we define biomes
+    static #number_of_food_spawn_points = 5;
+    static #food_per_spawn_min = 2;
+    static #food_per_spawn_max = 5;
+    
+    static {
 
-    for (var i = 0; i < number_of_food_spawn_points; i++) {
+        Events.Subscribe(Events.List.ChunkCreated, this.spawnFood.bind(this));
+    }
 
-        const spawnerPosition = {
-            x: Math.randomBetween(1, 30),
-            y: Math.randomBetween(1, 16),
-        }
-        const amountToSpawn = Math.randomBetween(food_per_spawn_min, food_per_spawn_max);
-        for(var i2 = 0; i2 < amountToSpawn; i2++) {
-            const offsetX = Math.randomBetween(-3, 3);
-            const offsetY = Math.randomBetween(-3, 3);
-
-            const opts = {
-                position: {
-                    x: spawnerPosition.x + offsetX,
-                    y: spawnerPosition.y + offsetY
-                }
-            };
-            
-            Object.assign(opts, CharacterType.Apple);
-            new Character(opts);
+    static spawnFood(chunk) {
+    
+        for (var i = 0; i < this.#number_of_food_spawn_points; i++) {
+    
+            const chunkX = chunk.x * Chunk.CHUNK_SIZE;
+            const chunkY = chunk.y * Chunk.CHUNK_SIZE;
+            const randomX = Math.randomBetween(1, Chunk.CHUNK_SIZE - 1);
+            const randomY = Math.randomBetween(1, Chunk.CHUNK_SIZE - 1);
+            const spawnerPosition = {
+                x: chunkX + randomX,
+                y: chunkY + randomY,
+            }
+            const amountToSpawn = Math.randomBetween(this.#food_per_spawn_min, this.#food_per_spawn_max);
+            for(var i2 = 0; i2 < amountToSpawn; i2++) {
+                const offsetX = Math.randomBetween(-3, 3);
+                const offsetY = Math.randomBetween(-3, 3);
+    
+                const opts = {
+                    position: {
+                        x: spawnerPosition.x + offsetX,
+                        y: spawnerPosition.y + offsetY
+                    }
+                };
+                
+                // Maybe we should have a "spawn item" shorthand function?
+                // or maybe "character type" should be an option we can pass into the character constructor?
+                Object.assign(opts, CharacterType.Apple);
+                new Character(opts);
+            }
         }
     }
-});
+}
