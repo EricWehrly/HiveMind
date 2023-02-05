edaabd203434ff7b2045cb6f7fbf341d37929da1
EricWehrly <eric@disruptiontheory.com>
2023-02-05 18:17:48 -0500

    test that doesn't work whatever node. i don't care.

---
+++ engine/test/util/javascript-extensions/copyPublicProperties.test.mjs
@@ -0,0 +1,14 @@
+import { describe, it } from 'node:test';
+import copyPublicProperties from '../../../js/util/javascript-extensions.js';
+
+// before you look, you should know that this doesn't work
+describe('copyPublicProperties', () => {
+
+    it('should copy from an object', () => {
+
+    });
+
+    // not private members
+
+    // not functions
+});
