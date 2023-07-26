3f5ac2d74dab36ba045ec10d90c6ef772c8b9c99
Eric Wehrly <eric@disruptiontheory.com>
2023-07-25 22:25:35 -0400

    clean up

--- js/entities/buildings.mjs
+++ js/entities/buildings.mjs
@@ -18,26 +18,32 @@ const Build = function(context) {
 
     const selectedBuilding = context?.menu?.selected?.context;
 
-    console.log("I gonna build this guy:");
-    console.log(selectedBuilding);
-
     // for now we can just spawn our first building
     // which will produce seeds, which eventually grow into fruit you can eat
 
     const player = Character.LOCAL_PLAYER;
+
+    const characterOpts = {
+        name: 'Seeder',
+        color: `blue`,  // player color
+        health: 15,
+        position: player.position,
+        _currentPurposeKey: 'grow'
+    };
+
+    /*
+    console.log("I gonna build this guy:");
+    console.log(selectedBuilding);
+    console.log('Compare to:');
+    console.log(characterOpts);
+    */
 
     // check cost (player can "afford") before building
     // instantiate the instance
 
     // TODO: from selected
     // TODO: Why does the seeder move?
-        const spawnedCharacter = new HiveMindCharacter({
-            name: 'Seeder',
-            color: `blue`,  // player color
-            health: 15,
-            position: player.position,
-            _currentPurposeKey: 'grow'
-        });
+        const spawnedCharacter = new HiveMindCharacter(characterOpts);
 }
 
 const UI_MENU_BUILDINGS = new Menu({
