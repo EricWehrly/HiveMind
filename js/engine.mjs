4ffe616f98c2cd239782065fd0a78057d1d1b1e9
Eric Wehrly <eric@disruptiontheory.com>
2023-07-26 22:30:34 -0400

    add parameters to chunk

--- js/engine.mjs
+++ js/engine.mjs
@@ -1,8 +1,10 @@
 import Camera from './camera.mjs';
+import Seed from './core/seed.mjs';
 import Client from './network/client.mjs';
 
 const Game = {
-    Camera: new Camera()
+    Camera: new Camera(),
+    Seed: new Seed(123456)
 };
 
 export default Game;
