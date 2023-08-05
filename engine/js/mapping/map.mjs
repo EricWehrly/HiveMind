5fd891cbfe75af1941f96c3b9b7e8d480b8b6aa5
EricWehrly <eric@disruptiontheory.com>
2023-08-05 18:49:46 -0400

    get nearby chunks when a player changes chunks

--- engine/js/mapping/map.mjs
+++ engine/js/mapping/map.mjs
@@ -34,20 +34,55 @@ export default class Map {
         
         Events.Subscribe(Events.List.CharacterCreated, this.#playerMoved.bind(this));
         Events.Subscribe(Events.List.PlayerMoved, this.#playerMoved.bind(this));
+        Events.Subscribe(Events.List.PlayerChunkChanged, this.#playerChunkChanged.bind(this));
     }
 
-    addChunk(chunk) {
-        this.#chunks[chunk.coordinate] = chunk;
+    // TODO: Allow differint starting points to be passed in
+    // should we have an option about whether to include the originating chunk?
+    getNearbyChunks(startingChunk, distance) {
+
+        if(startingChunk == null) return;
+        if(!distance) distance = 1;
+        distance = Math.abs(distance);
+
+        const nearbyChunks = [];
+        for(var deltaX = distance * -1; deltaX < distance; deltaX++) {
+            for(var deltaY = distance * -1; deltaY < distance; deltaY++) {
+                if(deltaX == 0 && deltaY == 0) continue;
+                const coordinate = (startingChunk.x + deltaX) + ","
+                    + (startingChunk.y + deltaY);
+                    // console.log(`Looking for chunk at ${coordinate}`);
+                const chunk = this.#chunks[coordinate];
+                // if distance to startingChunk is 1?
+                if(chunk) nearbyChunks.push(chunk);
+            }
+        }
+
+        console.log(`${nearbyChunks.length} chunks within ${distance} of chunk ${startingChunk}`);
+        return nearbyChunks;
+    }
+
+    // event has:
+    // character, from, to
+    #playerChunkChanged(event) {
+        console.log("New chunk!");
+        console.log(event);
+
+        const nearbyChunks = this.getNearbyChunks(event.from);
+
+        // deactivate chunks we're moving away from
+        // activate chunks we're moving toward
     }
 
     #playerMoved(event) {
         const character = event.character || event;
-        // console.log("'e moved, then.");
 
         const chunk = character.position.chunk;
 
         const edge = this.#closeToChunkEdge(character.position);
         
+        // if the player is approaching the edge of their chunk
+        // make sure there's another chunk next to it
         if(edge == "left") this.getChunk({
             x: chunk.x - 1,
             y: chunk.y
@@ -67,10 +102,6 @@ export default class Map {
             x: chunk.x,
             y: chunk.y + 1
         });
-
-
-        // if the player is approaching the edge of their chunk
-        // make sure there's another chunk next to it
     }
 
     #closeToChunkEdge(point) {
@@ -94,6 +125,10 @@ export default class Map {
         return false;
     }
 
+    addChunk(chunk) {
+        this.#chunks[chunk.coordinate] = chunk;
+    }
+
     getChunk(options) {
 
         if(options.x != null && options.y != null) {
@@ -108,10 +143,6 @@ export default class Map {
         } else {
             console.error(`Don't know how to look up chunk for ${options}`);
         }
-    }
-
-    createChunk() {
-
     }
 
     /**
