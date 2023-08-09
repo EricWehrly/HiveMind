3f237ff40358f65ac1bef643a2f9d5c51a066b11
EricWehrly <eric@disruptiontheory.com>
2023-08-09 02:52:13 -0400

    a few timing metrics

--- js/engine.mjs
+++ js/engine.mjs
@@ -1,10 +1,14 @@
 import Camera from './camera.mjs';
 import Seed from './core/seed.mjs';
 import Client from './network/client.mjs';
+import Events from './events.mjs';
 
 const Game = {
     Camera: new Camera(),
-    Seed: new Seed(123456)
+    Seed: new Seed(123456),
+    Timings: {
+        FirstLoad: performance.now()
+    }
 };
 
 export default Game;
@@ -12,3 +16,7 @@ export default Game;
 const playfield = document.createElement("div")
 playfield.id = "playfield";
 document.body.appendChild(playfield);
+
+Events.Subscribe(Events.List.GameStart, function() {
+    Game.Timings.GameStart = performance.now();
+});
