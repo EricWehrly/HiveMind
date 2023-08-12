3b1363d8f5c1e5043b22180670e92d6af911c36f
EricWehrly <eric@disruptiontheory.com>
2023-08-12 19:10:47 -0400

    i think this corrects bounded randoms

--- js/core/seed.mjs
+++ js/core/seed.mjs
@@ -13,12 +13,17 @@ export default class Seed {
         this.#seed = seed;
     }
     
+    // TODO: Multiplayer will make this problematic:
+    // https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript/19303725#comment54541128_19303725
+    // but remember though that we intentionally wanted players (clients) doing the generation each, and sharing to the network
+    // 'different' could mean variety
+    // also, screw it, this is just supposed to be a prototype
     Random(min, max) {
 
         // TODO: the 'if' should be 'isNumeric'?
         if(min != null && max != null) {
             
-            return (Math.random() * max) + min;
+            return (this.Random() * max) + min;
         } else {
 
             var x = Math.sin(this.#seed++) * 10000;
