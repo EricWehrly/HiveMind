0806f7a617718d86fec6cc491f3d04bab7cde4e7
EricWehrly <eric@disruptiontheory.com>
2023-08-21 22:17:38 -0400

    update resource on screen when its value is updated

--- engine/js/entities/resource.mjs
+++ engine/js/entities/resource.mjs
@@ -2,17 +2,25 @@ import Listed from "../baseTypes/listed.mjs";
 import Events from "../events.mjs";
 
 Events.List.ResourceCreated = "ResourceCreated";
+Events.List.ResourceValueChanged = "ResourceValueChanged";
 
 export default class Resource extends Listed {
 
-    #value;
+    #value = 0;
 
     get value() {
         return this.#value;
     }
 
     set value(newValue) {
+        const oldValue = this.#value;
         this.#value = newValue;
+
+        Events.RaiseEvent(Events.List.ResourceValueChanged, {
+            resource: this,
+            from: oldValue,
+            to: this.#value
+        });
     }
 
     constructor(options) {
