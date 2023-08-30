8f9d7e1f9392e339aa2dcf41efffbdb8c6c5284e
EricWehrly <eric@disruptiontheory.com>
2023-08-30 01:56:28 -0400

    move research creation into charactertype

--- js/entities/characterType.mjs
+++ js/entities/characterType.mjs
@@ -1,15 +1,23 @@
 import Unlock from "../../engine/js/unlocks.mjs";
+import Research from "../../engine/js/research.mjs";
 
 export default class CharacterType {
 
     #unlock = null;
 
+    #research;
+    get research() { return this.#research; }
+
     constructor(options) {
 
         // TODO: throw error if no name
 
-        if(this.research) {
-            this.research.name = this.name;
+        if(options.research) {
+            this.#research = new Research({
+                name: options.name,
+                ...options.research
+            });
+            delete options.research;
         }
 
         Object.assign(this, options);
