75eb3c0ccf7cd4ae827508eb245ac8234d575fb4
EricWehrly <eric@disruptiontheory.com>
2023-08-30 00:54:54 -0400

    clean up research

--- js/research.mjs
+++ js/research.mjs
@@ -11,13 +11,15 @@ export default class Research extends Listed {
     static #UI_MENU_RESEARCH;
 
     static DoResearch(context) {
-        console.log(context);
+
+        const selectedResearch = context?.menu?.selected?.context;
+        console.log(selectedResearch);
     }
 
     static {
 
         // we shouldn't have to do this but there's a whole ugly call stack down to UIElement if we don't
-        Events.Subscribe(Events.List.GameStart, function() {
+        Events.Subscribe(Events.List.GameStart, function () {
             Research.#UI_MENU_RESEARCH = new Menu({
                 screenZone: UIElement.SCREEN_ZONE.MIDDLE_RIGHT,
                 name: "Research",
@@ -38,15 +40,18 @@ export default class Research extends Listed {
 
     set enabled(value) {
 
-        if(value == true) this.#enabled = true;
-        Events.RaiseEvent(Events.List.ResearchEnabled, this);
+        if (value == true && this.enabled != true) {
+            this.#enabled = true;
 
-        // TODO: if this is the first enabled, also enable research menu
-        // (but not really, for a tech demo. empty research menu is fine)
+            Events.RaiseEvent(Events.List.ResearchEnabled, this);
 
-        Research.#UI_MENU_RESEARCH.addItem({
-            name: this.name
-        })
+            // TODO: if this is the first enabled, also enable research menu
+            // (but not really, for a tech demo. empty research menu is fine)
+
+            Research.#UI_MENU_RESEARCH.addItem({
+                name: this.name
+            })
+        }
     }
 
     constructor(options) {
@@ -54,4 +59,4 @@ export default class Research extends Listed {
     }
 }
 
-if(window) window.Research = Research;
+if (window) window.Research = Research;
