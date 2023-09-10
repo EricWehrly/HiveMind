6373aed41694d74d23720e281e0f802f8ecce66e
Eric Wehrly <eric@disruptiontheory.com>
2023-09-09 22:18:12 -0400

    now label updates and strength does it all too

--- js/characterStats.mjs
+++ js/characterStats.mjs
@@ -3,6 +3,7 @@ import Menu from "../engine/js/ui/menu.mjs";
 import KeyboardController from "./controls/keyboard-controller.mjs";
 import Resource from "../engine/js/entities/resource.mjs";
 import Character from "../engine/js/entities/character.mjs";
+import Events from "../engine/js/events.mjs";
 
 let strength = null,
 speed = null;
@@ -21,20 +22,42 @@ const characterMenu = new Menu({
     menuAction: characterMenuAction
 });
 
+const strengthLabel = characterMenu.addLabel({
+    name: 'Strength'
+});
+
+const speedLabel = characterMenu.addLabel({
+    name: 'Speed'
+});
+
 const makeStronger = function() {
 
+    const localPlayer = Character.LOCAL_PLAYER;
     if(!strength) {
-        const localPlayer = Character.LOCAL_PLAYER;
-        strength = localPlayer.getAttribute("Strength");        
+        strength = localPlayer.getAttribute("Strength");
+    }
+
+    let increment = 1;
+    let costProjection = this.cost;
+
+    // TODO: It will "look" better if the cost is updated when ctrl is depressed
+    const ctrl = localPlayer.controller.isKeyDown("Control");
+    if(ctrl) {
+        for(var i = 0; i < 10; i++) {
+            costProjection = Math.round(costProjection + Math.log(costProjection));
+            increment += 1;
+        }
     }
     
     // TODO: Move cost into attribute
     const food = Resource.Get("food")?.value || 0;
-    if(food > this.cost) {
-        Resource.Get("food").value -= this.cost;
-        this.cost = Math.round(this.cost + Math.log(this.cost));
-        strength.value += 1;
+    if(food > costProjection) {
+        Resource.Get("food").value -= costProjection;
+        this.cost = Math.round(costProjection + Math.log(costProjection));
+        strength.value += increment;
     }
+
+    strengthLabel.Element.innerHTML = `Strength: ${strength.value}`;
 }
 
 const makeFaster = function() {
@@ -63,18 +86,30 @@ const makeFaster = function() {
         this.cost = Math.round(costProjection + Math.log(costProjection));
         speed.value += increment;
     }
+
+    speedLabel.Element.innerHTML = `Speed: ${speed.value}`;
 }
 
-const strongerItem = characterMenu.addItem({
+characterMenu.addItem({
     name: 'Hit Harder',
     cost: 40,
     callback: makeStronger
 });
 
-const fasterItem = characterMenu.addItem({
+characterMenu.addItem({
     name: 'Run Faster',
     cost: 40,
     callback: makeFaster
 });
 
 KeyboardController.AddDefaultBinding("openMenu/character upgrades", "c");
+
+Events.Subscribe(Events.List.GameStart, function() {
+    
+    const localPlayer = Character.LOCAL_PLAYER;
+    
+    strength = localPlayer.getAttribute("Strength");
+    strengthLabel.Element.innerHTML = `Strength: ${strength.value}`;
+    speed = localPlayer.getAttribute("Speed");
+    speedLabel.Element.innerHTML = `Speed: ${speed.value}`;
+});
