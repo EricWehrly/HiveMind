d2d1dba3b4dbc6cff9633fa869f0ba558846ff58
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-05-31 14:06:27 -0400

    sanity check test

---
+++ test/hifemindCharacter/inheritance.test.ts
@@ -0,0 +1,50 @@
+import { expect } from '@jest/globals';
+import HiveMindCharacter from '../../js/entities/character-extensions.mjs';
+import NodeAI from '../../js/ai/node.mjs';
+
+jest.mock('@/engine/js/action.mjs', () => {
+    return {
+        List: {}
+    }
+});
+jest.mock('@/engine/js/mapping/map.mjs', () => ({
+    Map: {
+        getChunk: jest.fn().mockImplementation(() => {
+            return {
+                equals: () => { return false; }
+            };
+         }),
+    }
+}));
+
+jest.mock('@/engine/js/events.mjs', () => {
+    return {
+        __esModule: true, // this property makes it work
+        default: {
+            Subscribe: jest.fn().mockImplementation(() => { }),
+            RaiseEvent: jest.fn().mockImplementation(() => { }),
+            List: new Proxy({}, {
+                get: function(target, name) {
+                    return name;
+                },
+                set: function(target, name, value) {
+                    return true;  // Indicate that the assignment succeeded
+                }
+            })
+        }
+    };
+});
+
+describe('HivemindCharacter.ctor', () => {
+    
+    it('should support ai constructor argument', () => {
+        const character = new HiveMindCharacter({
+            ai: NodeAI
+        });
+        expect(character.ai).toBeDefined();
+        if (!(character.ai instanceof NodeAI)) {            
+            console.log(JSON.stringify(character.ai));
+        }
+        expect(character.ai instanceof NodeAI).toBeTruthy();
+    });
+});
