c6b4e0838e6065b3f0c4ad1d021b1745e7dcd480
EricWehrly <eric@disruptiontheory.com>
2024-07-27 02:23:43 -0400

    restructure how eqipment works, introducing a class to track attachments

--- engine/js/entities/equipment.ts
+++ engine/js/entities/equipment.ts
@@ -1,11 +1,65 @@
 import { TechnologyTypes } from "../TechnologyTypes";
 import Events from "../events";
+import { Defer } from "../loop.mjs";
 import Technology from "../technology";
-import Entity from "./character/Entity";
+import { Combatant } from "./character/Combatant";
 
 Events.List.EquipmentChanged = "EquipmentChanged"
+Events.List.CooldownComplete = "CooldownComplete";
 
-type EquipmentCollection = { [K in TechnologyTypes]?: Technology };
+export interface EquipmentChangedEvent {
+    from: Technology;
+    to: Technology;
+    equipped: EquippedTechnology;
+    type: TechnologyTypes;
+    character: Combatant;
+}
+
+export interface CooldownCompleteEvent {
+    equippedTechnology: EquippedTechnology;
+    character: Combatant;
+}
+
+type EquipmentCollection = { [K in TechnologyTypes]?: EquippedTechnology };
+
+export class EquippedTechnology {
+    private _equipment: Equipment;
+    private _technology: Technology;
+    private _lastFired: number;
+    private _ready: boolean;
+    private _readyAt: number;
+
+    get technology() { return this._technology; }
+    get ready() { return this._ready; }
+    get readyAt() { return this._readyAt; }
+
+    set lastFired(newValue: number) {
+        this._lastFired = newValue;
+        this._readyAt = this._lastFired + this._technology.delay;
+        this._ready = false;
+
+        Defer(() => {
+            let details: CooldownCompleteEvent = {
+                equippedTechnology: this,
+                character: this._equipment.character
+            };
+            this._ready = true;
+            Events.RaiseEvent(Events.List.CooldownComplete, details);
+        }, this._technology.delay);
+    }
+
+    constructor(technology: Technology, equipment: Equipment) {
+        this._technology = technology;
+        this._equipment = equipment;
+        this._lastFired = performance.now();
+        this._ready = false;
+        this._readyAt = 0;
+    }
+
+    get name() { return this._technology.name; }
+    get range() { return this._technology.range; }
+    get damage() { return this._technology.damage; }
+}
 
 export default class Equipment {
 
@@ -18,68 +72,39 @@ export default class Equipment {
 
     #buff: any = null;
     
-    // private _equipment: { [key: typeof techTypes]: Technology } = {};;
     private _equipment: EquipmentCollection = {};
 
-    constructor(character: Entity) {
-
-        // rather than doing as below, "Equipped" probably needs to be a partial class that gets &'d in ...
-
-        /*
-        character.getEquipped = function (techType) {
-            return this._equipment[techType];
-        }
-
-        character.hasEquipped = function (techType) {
-            return this.getEquipped(techType) != null;
-        }
-
-        character.equip = function (technology) {
-            this._equipment.equip(technology);
-        }
-        */
-
+    constructor(character: Combatant) {
         this.#character = character;
     }
 
-    get attack() {
-        return this._attack;
-    }
+    get attack() { return this._attack; }
 
-    set attack(newValue) {
-        this._attack = newValue;
-    }
+    set attack(newValue) { this._attack = newValue; }
 
-    get attackModifier() {
-        return this._attackModifier;
-    }
+    get attackModifier() { return this._attackModifier; }
 
-    set attackModifier(newValue) {
-        this._attackModifier = newValue;
-    }
+    set attackModifier(newValue) { this._attackModifier = newValue; }
 
     get buff() { return this.#buff; }
-    set buff(newValue) {
-        this.#buff = newValue;
-    }
+    set buff(newValue) { this.#buff = newValue; }
+
+    get character() { return this.#character; }
 
     equip(technology: Technology) {
+        
+        this._equipment[technology.type as TechnologyTypes] = new EquippedTechnology(technology, this);
 
-        const details = {
+        const details: EquipmentChangedEvent = {
             type: technology.type,
-            from: this.getEquipped(technology.type),
+            from: this.getEquipped(technology.type).technology,
             to: technology,
-            character: this.#character
+            character: this.#character,
+            equipped: this.getEquipped(technology.type)
         };
 
         Events.RaiseEvent(Events.List.EquipmentChanged, details);
-
-        // if we were to write a unit test here, it would say:
-        // assigned must be a new instance, not a reference to an existing one
-        // this[technology.type] = new Technology(technology);
-
-        // this._equipment[technology.type] = new Technology(technology);
-        this._equipment[technology.type as TechnologyTypes] = technology;
+        this._equipment[technology.type as TechnologyTypes].lastFired = performance.now();
     }
     
     getEquipped(techType: TechnologyTypes) {
