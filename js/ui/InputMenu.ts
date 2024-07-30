1a22fe9c012ee12f3006e8b0dbb7dde29c75801d
EricWehrly <eric@disruptiontheory.com>
2024-07-30 18:50:29 -0400

    fill out the input menu

--- js/ui/InputMenu.ts
+++ js/ui/InputMenu.ts
@@ -1,10 +1,17 @@
 // when hit escape
 
-import Menu from "../../engine/js/ui/menu";
+import Events from "../../engine/js/events";
+import Menu, { MenuItem } from "../../engine/js/ui/menu";
 import UIElement from "../../engine/js/ui/ui-element";
 import KeyboardController from "../controls/keyboard-controller.mjs";
 
 // show panel
+
+function reBindKey(action: string, key: string) {
+    console.log(action);
+    console.log(key);
+    // KeyboardController.AddDefaultBinding(action, key);
+}
 
 // (start by) listing everything in
 // KeyboardController.Default_Bindings
@@ -12,8 +19,21 @@ const characterMenu = new Menu({
     screenZone: UIElement.SCREEN_ZONE.MIDDLE_RIGHT,
     name: "Key Bindings",
     visible: false,
+    menuAction: reBindKey
 });
 
 KeyboardController.AddDefaultBinding("openMenu/key bindings", "Escape");
 
-// characterMenu.addItem()
+Events.Subscribe(Events.List.GameStart, function() {
+    for(const [action, key] of Object.entries(KeyboardController.Default_Bindings)) {
+        const menuItem: MenuItem = {
+            name: `${action} - ${key}`,
+            context: {
+                // callback: reBindKey
+                action,
+                key
+            }
+        }
+        characterMenu.addItem(menuItem);
+    }
+});
