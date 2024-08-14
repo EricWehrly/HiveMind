847d460f38cdf37c90580f8b108e8aaabe36e50d
EricWehrly <eric@disruptiontheory.com>
2024-08-14 02:04:28 -0400

    first pass at game config, wire into debug menu

--- js/ui/DebugMenu.ts
+++ js/ui/DebugMenu.ts
@@ -1,18 +1,36 @@
+import Configuration from "../../engine/js/core/Configuration";
 import MenuItem, { MenuItemType } from "../../engine/js/ui/MenuItem";
 import Menu from "../../engine/js/ui/menu";
 import { SCREEN_ZONE } from "../../engine/js/ui/ui-element";
 import KeyboardController from "../controls/keyboard-controller.mjs";
 
+interface MenuContext {
+    menu: Menu
+}
+
+function toggleLevelMetaDrawing() {
+    Configuration.Set("debug.drawLevelMeta", !Configuration.Get("debug.drawLevelMeta"));
+}
+
+function debugMenuAction(context: MenuContext) {
+    const item = context.menu.selected;
+    if(item.context.action) item.context.action();
+}
+
+// TODO: initial value from config
 const debugMenu = new Menu({
     screenZone: SCREEN_ZONE.MIDDLE_LEFT,
     name: "Debug",
-    // menuAction: openMenu,
+    menuAction: debugMenuAction
 });
 
 new MenuItem({
     menu: debugMenu,
     name: 'Draw metadata on level',
-    menuItemType: MenuItemType.Checkbox
+    menuItemType: MenuItemType.Checkbox,
+    context: {
+        action: toggleLevelMetaDrawing
+    }
     // TODO: action is toggle checkbox
 });
 
