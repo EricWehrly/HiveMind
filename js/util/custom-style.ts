6c81fdd583b3a95716f2e87a73545481cf308d85
EricWehrly <eric@disruptiontheory.com>
2024-08-14 13:39:11 -0400

    migrate smaller files to ts

--- js/util/custom-style.ts
+++ js/util/custom-style.ts
@@ -2,7 +2,7 @@ const CUSTOM_STYLE_NODE = document.createElement("style");
 document.head.appendChild(CUSTOM_STYLE_NODE);
 
 // we should add functionality for removing / updating, later
-export default function addCustomStyle(cssString) {
+export default function addCustomStyle(cssString: string) {
 
     const domElement = document.createTextNode(cssString);
     CUSTOM_STYLE_NODE.appendChild(domElement);
