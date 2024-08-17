e6a7aeb597a03d009b4e7c0d12277cdf5402f84f
EricWehrly <eric@disruptiontheory.com>
2024-08-17 12:54:05 -0400

    migrate engine dependencies to engine entrypoint, depollute game file

--- js/main.ts
+++ js/main.ts
@@ -3,6 +3,27 @@ import Seed from './core/seed';
 import Events from './events';
 import Map from './mapping/map';
 
+// functionality
+import './mapping/tile-manager';
+
+// hack to fix a dumb error caused by subscription to not-yet-declared event in ui-equipment
+import './entities/equipment';
+// and the same error again at the end of ui-equipment
+import './entities/character/mixins/Combative';
+
+// ui
+// we could change so that the root 'engine/ui/' index does the imports (and returns the current "UI" object)
+// but that doesn't buy us much, besides grouping the following two imports in a parent
+// and that's not worth much
+import './ui/ui-equipment';
+import './ui/ui-resource.mjs';
+
+// rendering
+import './rendering/entities/entity-graphics';
+import './rendering/ui/ui-element-renderer';
+import './rendering/ui/equipment-renderer';
+import './rendering/mapping/chunk-graphic';
+
 const Game = {
     Camera: new Camera(),
     Seed: new Seed(123456),
