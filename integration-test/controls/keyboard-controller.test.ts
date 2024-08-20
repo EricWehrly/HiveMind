d39d03b2364a8abd3b2241328943973febb1d00c
EricWehrly <eric@disruptiontheory.com>
2024-08-19 20:27:02 -0400

    remove leftover comments

--- integration-test/controls/keyboard-controller.test.ts
+++ integration-test/controls/keyboard-controller.test.ts
@@ -1,18 +1,8 @@
-// AddDefaultBinding
-// new KeyboardController
-// expect binding to be bound
-// simulate keypress
-// expect event to be raised
-
 import mockEvents from "../../engine/test/testHelpers/mockEvents";
 import Action from "../../engine/js/action";
 import KeyboardController from "../../js/controls/keyboard-controller.mjs";
 
 jest.mock('@/engine/js/events', () => mockEvents);
-
-// (post instantiation)
-// AddDefaultBinding
-// expect binding to be bound
 
 describe('KeyboardController', () => {
     describe('AddDefaultBinding', () => {
@@ -40,7 +30,7 @@ describe('KeyboardController', () => {
             const keyboardController = new KeyboardController();
             KeyboardController.AddDefaultBinding(testAction.name, testKey);
             keyboardController.handleKeyDown({ key: testKey });
-            
+
             expect(testActionCallback).toHaveBeenCalled();
         });
     });
