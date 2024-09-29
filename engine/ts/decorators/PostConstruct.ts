fa49b95d74727c053810a8f71d18a5cec705d30f
EricWehrly <eric@disruptiontheory.com>
2024-09-29 15:26:46 -0400

    get rid of unused

--- engine/ts/decorators/PostConstruct.ts
+++ engine/ts/decorators/PostConstruct.ts
@@ -6,11 +6,10 @@ export default function PostConstruct(target: any, method: Function) {
     if(!PostConstructMethods.has(target.name)) {
         PostConstructMethods.set(target.name, []);
     }
-    if(!target.constructor._postConstructMethods) {
-        target.constructor._postConstructMethods = [];
-    }
-    target.constructor._postConstructMethods.push(method);
-    PostConstructMethods.get(target.name).push(method);
+   const methods = PostConstructMethods.get(target.name);
+   if(!methods.includes(method)) {
+        PostConstructMethods.get(target.name).push(method);
+   }
 }
 
 export function RunPostConstructMethods(entity: Entity, classNames: string[]) {    
