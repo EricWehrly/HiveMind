40465f1117d5ec8715b1d33bb8f8598331f58681
EricWehrly <eric@disruptiontheory.com>
2024-10-06 07:52:54 -0400

    more sensible structure for timing storage

--- engine/js/util/Timings.ts
+++ engine/js/util/Timings.ts
@@ -1,48 +1,52 @@
-interface SegmentDictionary {
-    [key: string]: SegmentCollection;
-}
-
 type TimingSegment = { 
     start?: number; 
     end?: number; 
     time?: number;  // essentially cache the elapsed time for the segment, since that's the information we're mostly going to want to use
 };
 
-type SegmentOptions = {
+export type SegmentOptions = {
     type: string;
     keepTime?: number;
     keepCount?: number;
+    slowThreshold?: number;
 }
 
-type SegmentCollection = {
-    segments: TimingSegment[];
-    options: SegmentOptions;
+class SegmentCollection {
+    segments: TimingSegment[] = [];
+    readonly options: SegmentOptions;
+
+    // if options.keepCount
+    // use these vars to track 'state'
+    // (should we have a boolean that chooses if this is on?)
+    firstSegmentTimestamp: number;
+    segmentCount: number = 0;
+
+    constructor(options: SegmentOptions) {
+        this.options = options;
+    }
 }
 
 export default class Timing {
 
-    static Segments: SegmentDictionary = {};
+    static Segments: Record<string, SegmentCollection> = {};
     static SegmentKeys: Record<string, string> = {};
-    static Enabled: boolean = false;
+    static Enabled: boolean = true;
 
     static StartSegment(name: string, options: SegmentOptions) {
-        if (!this.Enabled) return;
+        if (!Timing.Enabled) return;
 
-        const segmentCollection = this._getSegmentCollection(name, options);
+        const segmentCollection = Timing._getSegmentCollection(name, options);
         segmentCollection.segments.push({
             start: performance.now()
         });
     }
 
-    private static _getSegmentCollection(name: string, options: SegmentOptions) {
-        if(!this.Segments.hasOwnProperty(name)) {
-            this.Segments[name] = {
-                segments: [],
-                options
-            };
+    private static _getSegmentCollection(name: string, options: SegmentOptions): SegmentCollection {
+        if(!Timing.Segments.hasOwnProperty(name)) {
+            Timing.Segments[name] = new SegmentCollection(options);
         }
 
-        return this.Segments[name];
+        return Timing.Segments[name];
     }
     
     // ends the last segment, rather than a specific one
@@ -50,18 +54,33 @@ export default class Timing {
 
         if(!Timing.Enabled) return;
 
-        const segmentCollection = this.Segments[name];
+        const segmentCollection = Timing.Segments[name];
+        const { keepCount, keepTime, slowThreshold } = segmentCollection.options;
         const segments = segmentCollection.segments;
         const index = segmentCollection.segments.length - 1;
         const segment = segments[index];
         segment.end = performance.now();
         segment.time = segment.end - segment.start;
 
-        if(segment.keepCount && segments.length > segment.keepCount) {
+        if(keepCount && segments.length > keepCount) {
             segments.splice(0, 1);
         }
 
-        // TODO: keepTime
+        if(keepTime) {
+            if(!segmentCollection.firstSegmentTimestamp) {
+                segmentCollection.firstSegmentTimestamp = segment.start;
+            } else {
+                if(performance.now() - segmentCollection.firstSegmentTimestamp > keepTime) {
+                    segmentCollection.segments = [];
+                    segmentCollection.firstSegmentTimestamp = null;
+                    segmentCollection.segmentCount = 0;
+                }
+            }
+        }
+
+        if(slowThreshold && segment.time < slowThreshold) {
+            segments.splice(index, 1);
+        }
     }
 
     static GetSegments() {
