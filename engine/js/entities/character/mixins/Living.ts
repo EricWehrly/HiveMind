233a982bc7d4a998afe16958519c57e8678e5e0b
EricWehrly <eric@disruptiontheory.com>
2024-10-08 19:44:07 -0400

    null is a valid and expected for health ... as weird as that actually is to think about

--- engine/js/entities/character/mixins/Living.ts
+++ engine/js/entities/character/mixins/Living.ts
@@ -1,6 +1,6 @@
 import CharacterType from "../../../../../js/entities/CharacterType";
 import Events, { GameEvent } from "../../../events";
-import Entity from "../Entity";
+import Entity, { EntityOptions } from "../Entity";
 
 Events.List.CharacterDamaged = "CharacterDamaged";
 Events.List.CharacterDied = "CharacterDied";
@@ -15,7 +15,7 @@ export interface Living {
 }
 
 export interface LivingOptions {
-    health: number;
+    health?: number;
     maxHealth?: number;
     characterType?: CharacterType;
 }
@@ -71,7 +71,7 @@ export function MakeLiving<T extends Constructor<Entity>>(Base: T) {
         constructor(...args: any) {
             super(...args);
 
-            const [ options ] = args;
+            const [options]: (EntityOptions & LivingOptions)[] = args;
 
             this._health = options?.health || options.characterType?.health || 0;
             this._initialHealth = this._health;
