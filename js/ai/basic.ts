bd6c5697cd79e3ab22a7f384deff00ea3ca3feca
EricWehrly <eric@disruptiontheory.com>
2024-10-10 22:11:13 -0400

    complete change to have AI use relationships to determine destination remove the concept of an ai having a target position

--- js/ai/basic.ts
+++ js/ai/basic.ts
@@ -7,7 +7,6 @@ import { Sentient } from "../entities/character/mixins/Sentient";
 import Events, { GameEvent } from "../events";
 import { Defer } from '../loop.mjs';
 
-const MS_BETWEEN_WANDER_DESTINATIONS = 30000;   // 30 seconds
 const MS_LEASH_COOLDOWN = 3000;
 
 Events.List.CharacterTargetChanged = "CharacterTargetChanged";
@@ -36,27 +35,13 @@ export default class AI {
 
     private _character: Entity & Sentient;
     private _leashing = false;
-    private _fleeing = false;
     private _relationships: Map<Entity, EntityRelationship> = new Map();
-    private _lastDestinationPickedTime = performance.now() - (MS_BETWEEN_WANDER_DESTINATIONS / 2);
     private _targetEntity: Entity;
-    private _targetPosition: WorldCoordinate;
-    private _spawnPosition: Readonly<WorldCoordinate>;
-    private _maxWanderDistance = 10;    // we can add setters & options setup later when we want to use them
     private thinkFunctions: ThinkFunction[] = [];
 
-    get leashing() { return this._leashing; }
     get character() { return this._character; }
-    get fleeing() { return this._fleeing; }
+    get leashing() { return this._leashing; }
     get targetEntity() { return this._targetEntity; }
-
-    // ideally, we want to only ever return a WorldCoordinate
-    // and with this if/else, we can support targeting either an entity, or a position
-    // (but this does imply that targeted entities are the 'priority')
-    get targetPosition() { 
-        if(this.targetEntity != null) return this.targetEntity.position;
-        return this._targetPosition;
-    }
 
     set targetEntity(newValue) { 
         
@@ -77,8 +62,6 @@ export default class AI {
     constructor(character: Entity & Sentient) {
         this._character = character;
 
-        this._spawnPosition = this._character.position;
-
         Events.Subscribe(Events.List.CharacterDamaged, this.onCharacterDamaged.bind(this));
     }    
 
@@ -98,49 +81,18 @@ export default class AI {
 
     think(elapsed: number) {
 
-        if(this._fleeing &&
-            this.isAtTarget() || this.isPastWanderLimits()
-        ) {
-            this._fleeing = false;
-        }
-        if (!this._fleeing && this._leashing == false) {
-
-            // if i don't have a target
-            this.wander();
-
-            // this prevents the character chasing the player (too far) as well
-            this.leash(this._spawnPosition, this._maxWanderDistance);
-        }
-
-        this._character.pointAtTarget(this.targetPosition);
+        this.wander();
 
         this.thinkFunctions.forEach(func => func(elapsed));
     }
 
-    private isPastWanderLimits(): boolean {
-        return this._character.position.distance(this._spawnPosition) > this._maxWanderDistance;
-    }
-
-    private isAtTarget() {
-        return this._character.position.distance(this.targetPosition) < 1;
-    }
-
     wander() {
         if(this.targetEntity) return;
-
-        if (performance.now() - this._lastDestinationPickedTime > MS_BETWEEN_WANDER_DESTINATIONS) {
-            /*
-            if (this?.character?.target?.position) {
-                console.debug(`Old target: ${this._character.target.position.x}, ${this._character.target.position.y}`);
-            }
-            */
-            this._lastDestinationPickedTime = performance.now();
-            // we need to influence this to avoid anything we're afraid of
-            // loop through our relationships and determine how 'strongly' we want to move in each direction
-            this._targetPosition = this.#randomTargetPosition();
-
-            // console.debug(`New target: ${this._character.targetPosition.x}, ${this._character.targetPosition.y}`);
-        }
+        
+        this.character.SetDesiredMovementVector(
+            this._desiredMovementVector.x,
+            this._desiredMovementVector.y
+        );
     }
 
     // TODO: mark private
@@ -164,27 +116,9 @@ export default class AI {
         return vector.normalized;
     }
 
-    #randomTargetPosition() {
-
-        let x, y;
-
-        var randX = Math.random();
-        if (randX > 0.5) x = (10 * randX);
-        else x = (-10 * randX);
-
-        var randY = Math.random();
-        if (randY > 0.5) y = (10 * randY);
-        else y = (-10 * randY);
-
-        return new WorldCoordinate(
-            this.character.position.x + x,
-            this.character.position.y + y);
-    }
-
     // ideally protected
     onCharacterDamaged(details: CharacterDamagedEvent) {
         if(details.character != this.character) return;
-        this.flee(details.attacker);
         this.setRelationship(details.attacker, { type: EntityRelationshipType.Afraid, amount: 1 });
     }
 
@@ -192,6 +126,7 @@ export default class AI {
         this._leashing = false;
     }
 
+    /* TODO: fix this
     leash(point: Readonly<WorldCoordinate>, distance: number) {
         var dist = this._character.position.distance(point);
         if (dist > distance) {
@@ -202,22 +137,5 @@ export default class AI {
             Defer(this.#unleash.bind(this), MS_LEASH_COOLDOWN);
         }
     }
-
-    flee(from: Entity) {
-        
-        let x, y;
-
-        const randX = Math.random();
-        if (randX > 0.5) x = 10;
-        else x = -10;
-        x += from.position.x;
-        
-        const randy = Math.random();
-        if (randy > 0.5) y = 10;
-        else y = -10;
-        y += from.position.y;
-
-        this._fleeing = true;
-        this._targetPosition = new WorldCoordinate(x, y);
-    }
+    */
 }
