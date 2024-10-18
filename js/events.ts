ebcfa00b08d92894363b7eb53402060d898a654d
EricWehrly <eric@disruptiontheory.com>
2024-10-17 20:22:53 -0400

    ui-resource to ts

--- js/events.ts
+++ js/events.ts
@@ -24,7 +24,8 @@ interface Subscription extends SubscribeOptions {
 }
 
 export interface GameEvent {
-    id: string;
+    // id is created in the generation -- garunteed when it's raised, but not needed when raising
+    id?: string;
     // by marking this definitively distinct property as non-nullable, we can browse errors
     // to try to find anywhere raising a GameEvent with an entity or other object that legitimately has an id property
     gameEventSpecificPropertyToForceInheritance?: boolean;
