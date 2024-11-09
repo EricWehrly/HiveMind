7665d19385740de903a74dde8d53a5f1b82d2a2e
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-11-09 01:24:39 -0500

    clean up

--- js/rendering/mapping/Tiles.renderer.ts
+++ js/rendering/mapping/Tiles.renderer.ts
@@ -19,35 +19,19 @@ export function RenderChunkTiles(context: CanvasRenderingContext2D, chunk: Chunk
             gridSize,
             gridSize
         );
-        context.fillStyle = "black";
-        context.font = "12px Arial";
-        // can we do this only if the mouse is over the tile?
 
         const rect = new Rectangle(
             startCoords.x + (tile.x * gridSize), 
             startCoords.y + (tile.y * gridSize),
             gridSize, gridSize);
         const mouse = Renderer.MouseScreenPos;
-        // TODO: simpler math
         if(mouse && rect && rect.containsPoint(mouse.x, mouse.y)) {
+            context.fillStyle = "black";
+            context.font = "12px Arial";
             context.fillText(
-                // `${tile.x},${tile.y},${tile.z}`,
-                // `${tile.x},${tile.y}`,
-                `${rect.x}`,
+                `${tile.x},${tile.y}`,
                 rect.x + 4, 
                 rect.y + 14);
-
-                context.fillText(
-                    `${rect.y}`,
-                    rect.x + 4, 
-                    rect.y + 28);
-
-                /*
-                context.fillText(
-                    `${startCoords.y}`,
-                    startCoords.x + (tile.x * gridSize) + 4, 
-                    startCoords.y + (tile.y * gridSize) + 20);
-                    */
         }
     }
 }
